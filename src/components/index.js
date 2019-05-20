36ddb0f50b1613c65a164a1314cfb095b73c571d
Benjamin Christau <benjamin.christau@oyez.fr>
2019-05-20 19:13:59 +0200

    Add missing exports in components

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ import Header from 'components/Header';
 import Drawer from 'components/Drawer';
 import Content from 'components/Content';
 import Loader from 'components/Loader';
+import utils from 'components/utils';
 
 export { default as App } from 'components/App';
 export { default as Page } from 'components/Page';
@@ -11,6 +12,7 @@ export { default as Header } from 'components/Header';
 export { default as Drawer } from 'components/Drawer';
 export { default as Content } from 'components/Content';
 export { default as Loader } from 'components/Loader';
+export { default as utils } from 'components/utils';
 
 export const Components = {
   App,
@@ -19,6 +21,7 @@ export const Components = {
   Drawer,
   Content,
   Loader,
+  utils,
 };
 
 export default Components;
